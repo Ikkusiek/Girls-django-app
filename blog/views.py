dc2d23739c9d19734cdb64295d9b1a27ce935498
Kasia <ikkusiek@gmail.com>
2017-11-13 23:48:21 +0100

    modyfikacja szablonu, aby pokazywał posty z bazy danych

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+__author__='admin'
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+  #  posts = Post.objects.filter(author=admin) #zmienna post, nazwa naszego querysetu
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
